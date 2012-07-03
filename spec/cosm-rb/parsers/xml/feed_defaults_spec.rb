dcb030a5f15b00660e620b77ea8c5db3256d01e7
Sam Mulube <sam@pachube.com>
2012-07-03 00:46:55 +0000

    Added spec for the breaking XML parser issue.
    Handles dodgy tags in XML better.

--- spec/cosm-rb/parsers/xml/feed_defaults_spec.rb
+++ spec/cosm-rb/parsers/xml/feed_defaults_spec.rb
@@ -60,6 +60,27 @@ describe "default feed xml parser" do
       Cosm::Feed.new(@xml).should fully_represent_feed(:xml, @xml)
     end
 
+    it "should handle awkward tags" do
+      xml = <<-EOXML
+<?xml version="1.0" encoding="UTF-8"?>
+<eeml version="5" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.eeml.org/xsd/005" xsi:schemaLocation="http://www.eeml.org/xsd/005 http://www.eeml.org/xsd/005/005.xsd">
+  <environment>
+    <data id="4773635294300160">
+      <tag>
+        10.F59894010800</tag>
+      <tag>
+        Living Room, S wall</tag>
+      <value>
+        24.1875</value>
+      <unit type="derivedSI">
+        celsius</unit>
+    </data>
+  </environment>
+</eeml>
+EOXML
+      feed = Cosm::Feed.new(xml)
+      feed.datastreams.first.tags.should == "10.F59894010800,\"Living Room, S wall\""
+    end
   end
 end
 
